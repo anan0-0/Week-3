9a00b0acab7745645a215ad1072c9f98ecd3c357
Anan <ranan22@tbc.edu.np>
2022-11-02 11:41:52 +0545

    Q.2

---
+++ Q.2.py
@@ -0,0 +1,10 @@
+# Write a program that simulates the way in which a user might choose a password.
+# The program should prompt for a new password, and then prompt again. If the two
+# passwords entered are the same the program should say "Password Set" or
+# similar, otherwise it should report an error.
+pw=input("Enter the password: ")
+pw1=input("Enter the password again: ")
+if pw==pw1:
+    print("Password set")
+else:
+    print("Password does not match")
